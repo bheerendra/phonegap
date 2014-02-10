a4f7c8fd6d82c89714c1cfcaf4f6e34ca9da9648
Bheerendra <bheerendra@gmail.com>
2014-02-11 01:30:40 +0530

    First Commit

---
+++ Test2/src/org/apache/cordova/example/cordovaExample.java
@@ -0,0 +1,15 @@
+package org.apache.cordova.example;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class cordovaExample extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
+
